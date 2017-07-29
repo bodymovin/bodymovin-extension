4a00ed1c324ba31cd8a6a13a4332ad85ba58b0af
hernan <hernantorrisi@gmail.com>
2017-07-29 17:00:43 -0300

    version update

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var replace = require('gulp-replace');
 var gzip = require('gulp-gzip');
 var insert = require('gulp-insert');
 
-var version = '4.9.0'
+var version = '4.9.1'
 
 var extensionSource = './bundle';
 var extensionDestination = '../../../Users/Deeandra/AppData/Roaming/Adobe/CEP/extensions/bodymovin';
