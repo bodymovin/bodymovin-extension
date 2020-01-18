2863339eadc5a551e9a905b98aeb4791effcbf62
airnan <hernantorrisi@gmail.com>
2020-01-18 12:46:19 -0500

    version 5.6.2

---
+++ src/helpers/importers/lottie/text.js
@@ -0,0 +1,56 @@
+import random from '../../randomGenerator'
+import sendCommand from './commandHelper'
+
+const getTextDocumentData = (textDocumentData) => {
+	if ('k' in textDocumentData) {
+		return textDocumentData.k
+	} else {
+		console.log('textDocumentData', textDocumentData)
+		return [
+			{
+				s: textDocumentData
+			}
+		]
+	}
+}
+
+const processText = (textData, layerId) => {
+	const textDocumentData = getTextDocumentData(textData.d)
+	const sourceTextIdId = random(10);
+	sendCommand('assignIdToProp', ['Source Text', sourceTextIdId, layerId]);
+	if (textDocumentData.length === 1) {
+		const textDocumentValue = textDocumentData[0].s
+		sendCommand('setTextDocumentValue',
+			[
+				layerId, 
+				encodeURIComponent(textDocumentValue.t),
+				textDocumentValue.s,
+				encodeURIComponent(textDocumentValue.f),
+				textDocumentValue.fc,
+				textDocumentValue.tr,
+				textDocumentValue.j,
+				textDocumentValue.ls || 0,
+			]
+		);
+	} else {
+		textDocumentData.forEach(textDocument => {
+			const textDocumentValue = textDocument.s
+			sendCommand('setTextDocumentValueAtTime',
+				[
+					layerId,
+					textDocument.t,
+					encodeURIComponent(textDocumentValue.t),
+					textDocumentValue.s,
+					encodeURIComponent(textDocumentValue.f),
+					textDocumentValue.fc,
+					textDocumentValue.tr,
+					textDocumentValue.j,
+					textDocumentValue.ls || 0,
+				]
+			);
+		})
+	}
+	// encodeURIComponent(textData.d.k[0].s.t)
+}
+
+export default processText
