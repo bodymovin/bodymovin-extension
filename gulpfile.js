ccb12ef50b872cf713fd4090b6a81de0a7fc98fb
airnan <hernantorrisi@gmail.com>
2020-02-11 21:10:30 -0500

    version 5.6.4

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var replace = require('gulp-replace');
 var gzip = require('gulp-gzip');
 var insert = require('gulp-insert');
 
-var version = '5.6.3'
+var version = '5.6.4'
 
 var extensionSource = './bundle';
 var extensionDestination = '../../../tropi/AppData/Roaming/Adobe/CEP/extensions/bodymovin';
