ff010dd2928d2c3387fe7bb611c9bfa04c3a55e5
Florin Malita <fmalita@gmail.com>
2020-03-20 13:27:50 -0400

    Export additional layer styles
      - Drop Shadow
      - Inner Shadow
      - Outer Glow
      - Inner Glow

--- bundle/jsx/utils/layerStylesHelper.jsx
+++ bundle/jsx/utils/layerStylesHelper.jsx
@@ -4,13 +4,25 @@ $.__bodymovin.bm_layerStylesHelper = (function () {
     var bm_keyframeHelper = $.__bodymovin.bm_keyframeHelper;
     var ob = {};
     var layerStyleTypes = {
-        stroke: 0
+        stroke: 0,
+        dropShadow: 1,
+        innerShadow: 2,
+        outerGlow: 3,
+        innerGlow: 4
     };
     
     function getStyleType(name) {
         switch (name) {
         case 'frameFX/enabled':
             return layerStyleTypes.stroke;
+        case 'dropShadow/enabled':
+            return layerStyleTypes.dropShadow;
+        case 'innerShadow/enabled':
+            return layerStyleTypes.innerShadow;
+        case 'outerGlow/enabled':
+            return layerStyleTypes.outerGlow;
+        case 'innerGlow/enabled':
+            return layerStyleTypes.innerGlow;
         default:
             return '';
         }
@@ -18,10 +30,96 @@ $.__bodymovin.bm_layerStylesHelper = (function () {
     
     function exportStroke(style, frameRate, stretch) {
         var ob = {};
-        ob.ty = layerStyleTypes.stroke;
-        ob.nm = style.name;
         ob.c = bm_keyframeHelper.exportKeyframes(style.property('frameFX/color'), frameRate, stretch);
         ob.s = bm_keyframeHelper.exportKeyframes(style.property('frameFX/size'), frameRate, stretch);
+        return ob;
+    }
+    
+    function exportDropShadow(style, frameRate, stretch) {
+        var ob = {};
+        // Color
+        ob.c = bm_keyframeHelper.exportKeyframes(style.property('dropShadow/color'), frameRate, stretch);
+        // Opacity
+        ob.o = bm_keyframeHelper.exportKeyframes(style.property('dropShadow/opacity'), frameRate, stretch);
+        // Angle
+        ob.a = bm_keyframeHelper.exportKeyframes(style.property('dropShadow/localLightingAngle'), frameRate, stretch);
+        // Size
+        ob.s = bm_keyframeHelper.exportKeyframes(style.property('dropShadow/blur'), frameRate, stretch);
+        // Distance
+        ob.d = bm_keyframeHelper.exportKeyframes(style.property('dropShadow/distance'), frameRate, stretch);
+        // Choke/Spread
+        ob.ch = bm_keyframeHelper.exportKeyframes(style.property('dropShadow/chokeMatte'), frameRate, stretch);
+        // Blend Mode
+        ob.bm = bm_keyframeHelper.exportKeyframes(style.property('dropShadow/mode2'), frameRate, stretch);
+        // Noise
+        ob.no = bm_keyframeHelper.exportKeyframes(style.property('dropShadow/noise'), frameRate, stretch);
+        // Layer Knocks Out Drop Shadow
+        ob.lc = bm_keyframeHelper.exportKeyframes(style.property('dropShadow/layerConceals'), frameRate, stretch);
+        return ob;
+    }
+    
+    function exportInnerShadow(style, frameRate, stretch) {
+        var ob = {};
+        // Color
+        ob.c = bm_keyframeHelper.exportKeyframes(style.property('innerShadow/color'), frameRate, stretch);
+        // Opacity
+        ob.o = bm_keyframeHelper.exportKeyframes(style.property('innerShadow/opacity'), frameRate, stretch);
+        // Angle
+        ob.a = bm_keyframeHelper.exportKeyframes(style.property('innerShadow/localLightingAngle'), frameRate, stretch);
+        // Size
+        ob.s = bm_keyframeHelper.exportKeyframes(style.property('innerShadow/blur'), frameRate, stretch);
+        // Distance
+        ob.d = bm_keyframeHelper.exportKeyframes(style.property('innerShadow/distance'), frameRate, stretch);
+        // Choke/Spread
+        ob.ch = bm_keyframeHelper.exportKeyframes(style.property('innerShadow/chokeMatte'), frameRate, stretch);
+        // Blend Mode
+        ob.bm = bm_keyframeHelper.exportKeyframes(style.property('innerShadow/mode2'), frameRate, stretch);
+        // Noise
+        ob.no = bm_keyframeHelper.exportKeyframes(style.property('innerShadow/noise'), frameRate, stretch);
+        return ob;
+    }
+    
+    function exportOuterGlow(style, frameRate, stretch) {
+        var ob = {};
+        // Color
+        ob.c = bm_keyframeHelper.exportKeyframes(style.property('outerGlow/color'), frameRate, stretch);
+        // Opacity
+        ob.o = bm_keyframeHelper.exportKeyframes(style.property('outerGlow/opacity'), frameRate, stretch);
+        // Size
+        ob.s = bm_keyframeHelper.exportKeyframes(style.property('outerGlow/blur'), frameRate, stretch);
+        // Range
+        ob.r = bm_keyframeHelper.exportKeyframes(style.property('outerGlow/inputRange'), frameRate, stretch);
+        // Choke/Spread
+        ob.ch = bm_keyframeHelper.exportKeyframes(style.property('outerGlow/chokeMatte'), frameRate, stretch);
+        // Blend Mode
+        ob.bm = bm_keyframeHelper.exportKeyframes(style.property('outerGlow/mode2'), frameRate, stretch);
+        // Noise
+        ob.no = bm_keyframeHelper.exportKeyframes(style.property('outerGlow/noise'), frameRate, stretch);
+        // Jitter
+        ob.j = bm_keyframeHelper.exportKeyframes(style.property('outerGlow/shadingNoise'), frameRate, stretch);
+        return ob;
+    }
+    
+    function exportInnerGlow(style, frameRate, stretch) {
+        var ob = {};
+        // Color
+        ob.c = bm_keyframeHelper.exportKeyframes(style.property('innerGlow/color'), frameRate, stretch);
+        // Opacity
+        ob.o = bm_keyframeHelper.exportKeyframes(style.property('innerGlow/opacity'), frameRate, stretch);
+        // Size
+        ob.s = bm_keyframeHelper.exportKeyframes(style.property('innerGlow/blur'), frameRate, stretch);
+        // Range
+        ob.r = bm_keyframeHelper.exportKeyframes(style.property('innerGlow/inputRange'), frameRate, stretch);
+        // Source
+        ob.sr = bm_keyframeHelper.exportKeyframes(style.property('innerGlow/innerGlowSource'), frameRate, stretch);
+        // Choke/Spread
+        ob.ch = bm_keyframeHelper.exportKeyframes(style.property('innerGlow/chokeMatte'), frameRate, stretch);
+        // Blend Mode
+        ob.bm = bm_keyframeHelper.exportKeyframes(style.property('innerGlow/mode2'), frameRate, stretch);
+        // Noise
+        ob.no = bm_keyframeHelper.exportKeyframes(style.property('innerGlow/noise'), frameRate, stretch);
+        // Jitter
+        ob.j = bm_keyframeHelper.exportKeyframes(style.property('innerGlow/shadingNoise'), frameRate, stretch);
         return ob;
     }
     
@@ -35,12 +133,34 @@ $.__bodymovin.bm_layerStylesHelper = (function () {
         var stylesArray = [];
         for (i = 0; i < len; i += 1) {
             styleElement = styles(i + 1);
-            if (styleElement.isModified) {
+            if (styleElement.enabled) {
+                var styleOb = null;
                 var styleType = getStyleType(styleElement.matchName);
+                
                 switch (styleType) {
                 case layerStyleTypes.stroke:
-                    stylesArray.push(exportStroke(styleElement, frameRate, stretch));
+                    styleOb = exportStroke(styleElement, frameRate, stretch);
                     break;
+                case layerStyleTypes.dropShadow:
+                    styleOb = exportDropShadow(styleElement, frameRate, stretch);
+                    break;
+                case layerStyleTypes.innerShadow:
+                    styleOb = exportInnerShadow(styleElement, frameRate, stretch);
+                    break;
+                case layerStyleTypes.outerGlow:
+                    styleOb = exportOuterGlow(styleElement, frameRate, stretch);
+                    break;
+                case layerStyleTypes.innerGlow:
+                    styleOb = exportInnerGlow(styleElement, frameRate, stretch);
+                    break;
+                }
+                
+                if (styleOb) {
+                    // common props
+                    styleOb.ty = styleType;
+                    styleOb.nm = styleElement.name;
+                
+                    stylesArray.push(styleOb);
                 }
             }
         }
