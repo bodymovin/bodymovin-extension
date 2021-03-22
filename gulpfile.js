24fe2f00b13f8fe149b51b1750f669c5987ea77f
airnan <hernantorrisi@gmail.com>
2021-03-21 20:15:01 -0400

    version 5.7.7

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var replace = require('gulp-replace');
 var gzip = require('gulp-gzip');
 var insert = require('gulp-insert');
 
-var version = '5.7.6'
+var version = '5.7.7'
 
 var extensionSource = './bundle';
 var extensionDestination = '../../../tropi/AppData/Roaming/Adobe/CEP/extensions/bodymovin';
